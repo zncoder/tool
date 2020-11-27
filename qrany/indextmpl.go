e02fdbf9cfa011527900bf830f3851e5e7672f35
Zhang Bin <zncoder@gmail.com>
2020-11-26 17:26:21 -0800

    qrany

---
+++ qrany/indextmpl.go
@@ -0,0 +1,45 @@
+package main
+
+const indexTmpl = `<html>
+  <head>
+    <meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Download Files</title>
+    <style>
+      .done { text-decoration: line-through; }
+    </style> 
+  </head>
+  <body>
+    <div><a href="#" onclick="downloadAll()">Download all files</a></div>
+    <ol>
+      {{range .}}<li>
+				<a class="file" href="/download?f={{.Path}}" download="{{.Name}}">{{.Name}}</a>
+			</li>
+      {{end -}}
+		</ol>
+		
+    <script>
+			function downloadAll() {
+        let els = document.querySelectorAll(".file")
+        for (let el of els) {
+          download(el) 
+          el.classList.add("done")
+				}
+			}
+      
+      async function download(el) {
+				el.click()
+				await downloadComplete(el.href)
+			}
+
+			async function downloadComplete(href) {
+				let resp = await fetch("/status")
+				if (!resp.ok) {
+					throw href
+				}
+        console.log(await resp.text())
+			}
+    </script>
+  </body>
+</html>
+`
---
+++ qrany/qrany.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/mdp/qrterminal"
+	"github.com/zncoder/assert"
+	"github.com/zncoder/easycmd"
+)
+
+func main() {
+	easycmd.Handle("download", runDownload, "embed the download page in QR")
+	easycmd.Handle("snippet", runSnippet, "embed the snippet in QR")
+	easycmd.Main()
+}
+
+func runDownload() {
+	port := flag.Int("p", 10030, "port")
+	flag.Parse()
+	assert.OK(flag.NArg() > 0)
+
+	type FileInfo struct {
+		Name string
+		Path string
+	}
+	var files []FileInfo
+	for _, f := range flag.Args() {
+		files = append(files, FileInfo{Name: filepath.Base(f), Path: f})
+	}
+
+	status := make(chan string, 1)
+
+	var wg sync.WaitGroup
+	wg.Add(len(files))
+
+	tmpl := template.Must(template.New("index").Parse(indexTmpl))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		err := tmpl.Execute(w, files)
+		assert.Nil(err)
+	})
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		filename := r.URL.Query().Get("f")
+		f, err := os.Open(filename)
+		if err != nil {
+			http.Error(w, "open file", http.StatusNotFound)
+			return
+		}
+		defer f.Close()
+		w.Header().Add("Content-Type", "application/octet-stream")
+		_, err = io.Copy(w, f)
+		if err != nil {
+			log.Printf("download file:%q err:%v", filename, err)
+			return
+		}
+		status <- filename
+		wg.Done()
+		log.Println("finish", filename)
+	})
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		x := <-status
+		io.WriteString(w, x)
+	})
+
+	ip := HostIP()
+	lr, err := net.Listen("tcp", fmt.Sprintf("%v:%d", ip, *port))
+	assert.Nil(err)
+	go http.Serve(lr, nil)
+
+	addr := fmt.Sprintf("http://%v/", lr.Addr())
+	log.Printf("Listening on %s", addr)
+	qrterminal.Generate(addr, qrterminal.L, os.Stdout)
+	wg.Wait()
+	lr.Close()
+	time.Sleep(10 * time.Second)
+}
+
+func HostIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	assert.Nil(err)
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP
+}
+
+func runSnippet() {
+	flag.Parse()
+	assert.OK(flag.NArg() > 0)
+
+	s := strings.Join(flag.Args(), " ")
+	qrterminal.Generate(s, qrterminal.L, os.Stdout)
+}
